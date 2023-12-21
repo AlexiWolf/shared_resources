e0ac5d65c0cc2aaec55ea3cac6d70d2e7a357914
AlexiWolf <alexi@wolvesin.space>
2023-12-21 17:36:03 -0600

    Added docs to `AccessError`

--- src/errors.rs
+++ src/errors.rs
@@ -1,5 +1,9 @@
+/// Represents an error that occurred when accessing the [`Resources`](crate::Resources) store.
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum AccessError {
+    /// There is no [`Resource`](crate::Resource) of the requested type in the store.
     NoSuchResource,
+
+    /// The requested [`Resource`](crate::Resource) has already been borrowed.
     AlreadyBorrowed,
 }
