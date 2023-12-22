b84b308a91d649b2403a8cfbf8e8827a26eeab83
AlexiWolf <alexi@wolvesin.space>
2023-12-22 07:00:52 -0600

    Ran cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -13,23 +13,23 @@
 //!
 //! // Create a new resource store.
 //! let mut resources = Resources::default();
-//! 
+//!
 //! // Add a resource to the store.
 //! // The resource store will take ownership over the resource.
 //! resources.insert(resource);
 //!
 //! // An immutable borrow.
 //! {
-//!     // Resources::get() will return an AccessError if: 
+//!     // Resources::get() will return an AccessError if:
 //!     // - The requested type does not exist in the store.
 //!     // - There's an existing mutable borrow.
 //!     let resource = resources.get::<ExampleResource>()?;
 //!     assert_eq!(resource.0, "Hello, World!");
 //! }
-//! 
+//!
 //! // A mutable borrow.
 //! {
-//!     // Resources::get_mut() will return an AccessError if: 
+//!     // Resources::get_mut() will return an AccessError if:
 //!     // - The requested type does not exist in the store.
 //!     // - There's an existing mutable, or immutable borrow.
 //!     let mut resource = resources.get_mut::<ExampleResource>()?;
