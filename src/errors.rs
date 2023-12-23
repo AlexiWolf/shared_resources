b4f46338b9b48d6b87c1294b61deaaace721552e
AlexiWolf <alexi@wolvesin.space>
2023-12-22 19:54:11 -0600

    Added `Display` impl for `AccessError`

--- src/errors.rs
+++ src/errors.rs
@@ -7,3 +7,12 @@ pub enum AccessError {
     /// The requested [`Resource`](crate::Resource) has already been borrowed.
     AlreadyBorrowed,
 }
+
+impl std::fmt::Display for AccessError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AccessError::NoSuchResource => write!(f, "There is no resource of the requested type."),
+            AccessError::AlreadyBorrowed => write!(f, "The requested resource is already borrowed."),
+        }
+    }
+}
