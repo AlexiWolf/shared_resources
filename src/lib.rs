45cc8a3cf621bc2f2ca9707cf3b7c992b63f0203
AlexiWolf <alexi@wolvesin.space>
2023-12-24 16:11:32 -0600

    Added the rest of the description from the read-me

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
 //! This crate provides a shared resources container which is thread-safe, and lock-free.
 //!
+//! The [`Resources`] struct, is a container of [`Resource`] objects. Resources are inserted at 
+//! run-time. The container stores up to 1 instance of each type.  Stored resources can be accessed
+//! by the rest of the system through an immutable reference. Borrowing rules are checked at 
+//! run-time.
+//!
+//! Thread-safe access is provided by the [`ResourcesSync`] struct.  It's similar to the 
+//! [`Resources`] struct, except it only allows access to thread-safe resources, and can, itself, 
+//! be sent to other threads.
+//!
 //! # Examples
 //!
 //! ```
