c574bea8ec9d5bea5e79ff4f5a9d6b3017c98181
AlexiWolf <alexi@wolvesin.space>
2023-12-24 16:21:16 -0600

    Ran cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 //! Provides a shared resources container which is thread-safe, and lock-free.
 //!
-//! The [`Resources`] struct, is a container of [`Resource`] objects. Resources are inserted at 
+//! The [`Resources`] struct, is a container of [`Resource`] objects. Resources are inserted at
 //! run-time. The container stores up to 1 instance of each type.  Stored resources can be accessed
-//! by the rest of the system through an immutable reference. Borrowing rules are checked at 
+//! by the rest of the system through an immutable reference. Borrowing rules are checked at
 //! run-time.
 //!
-//! Thread-safe access is provided by the [`ResourcesSync`] struct.  It's similar to the 
-//! [`Resources`] struct, except it only allows access to thread-safe resources, and can, itself, 
+//! Thread-safe access is provided by the [`ResourcesSync`] struct.  It's similar to the
+//! [`Resources`] struct, except it only allows access to thread-safe resources, and can, itself,
 //! be sent to other threads.
 //!
 //! # Examples
@@ -56,9 +56,9 @@
 //!
 //! ## Multi-threaded Use
 //!
-//! The default [`Resources`] container is not [`Send`], or [`Sync`], so it *cannot* be sent 
-//! between threads.  Instead, you must create a [`ResourcesSync`], which is a thread-safe handle 
-//! to the resources container that can be sent to other threads.  Any resource accessed through 
+//! The default [`Resources`] container is not [`Send`], or [`Sync`], so it *cannot* be sent
+//! between threads.  Instead, you must create a [`ResourcesSync`], which is a thread-safe handle
+//! to the resources container that can be sent to other threads.  Any resource accessed through
 //! the handle must, itself, be [`Send`] / [`Sync`].
 //!
 //! ```
